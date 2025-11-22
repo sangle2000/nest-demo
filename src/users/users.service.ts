fa65b965adfca39f15e66e6a4e47accc7d52d79c
sangle2000 <thanhsang22440011@gmail.com>
2025-11-23 00:28:11 +0700

    Update

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -13,20 +13,28 @@ export class UsersService {
     return this.repo.save(user);
   }
 
-  findOne(id: number) {
-    return this.repo.findOne({ where: { id } });
+  async findOne(id: number) {
+    const user = await this.repo.findOne({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
   }
 
-  find(email: string) {
-    return this.repo.find({ where: { email } });
+  async find(email: string) {
+    const user = await this.repo.find({ where: { email } });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
   }
 
   async update(id: number, attrs: Partial<User>) {
     const user = await this.findOne(id);
-
-    if (!user) {
-      throw new Error('User not found');
-    }
 
     Object.assign(user, attrs);
 
@@ -35,10 +43,6 @@ export class UsersService {
 
   async remove(id: number) {
     const user = await this.findOne(id);
-
-    if (!user) {
-      throw new Error('User not found');
-    }
 
     return this.repo.remove(user);
   }
